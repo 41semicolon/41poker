1cc3bf60aa69ffed7e60d07c6a5bff1c425aba89
41semicolon <41semicolon@gmail.com>
2018-05-01 00:41:17 +0000

    added hrepr and hparse(), the hole card stuff.

--- src/card.js
+++ src/card.js
@@ -1,6 +1,19 @@
 /* eslint no-bitwise: 'off' */
 
 const card = (rank, suit) => (suit | (rank << 2));
+
+// single card stuff
+const rankRepr = (num) => {
+  switch (num >> 2) {
+    case 12: return 'A';
+    case 11: return 'K';
+    case 10: return 'Q';
+    case 9: return 'J';
+    case 8: return 'T';
+    default: return (num >> 2) + 2;
+  }
+};
+
 const repr = (num) => {
   let suitStr;
   switch (num & 0b11) {
@@ -10,16 +23,7 @@ const repr = (num) => {
     case 3: suitStr = '♠'; break;
     default:
   }
-  let rankStr;
-  switch (num >> 2) {
-    case 12: rankStr = 'A'; break;
-    case 11: rankStr = 'K'; break;
-    case 10: rankStr = 'Q'; break;
-    case 9: rankStr = 'J'; break;
-    case 8: rankStr = 'T'; break;
-    default: rankStr = (num >> 2) + 2;
-  }
-  return `${rankStr}${suitStr}`;
+  return `${rankRepr(num)}${suitStr}`;
 };
 const parse = (str) => {
   let rank;
@@ -50,4 +54,34 @@ const parse = (str) => {
   return card(rank, suit);
 };
 
-module.exports = { card, repr, parse };
+// hole cards stuff
+
+const hrepr = (cards) => { // cards -> AA, AKs, ...
+  const work = [...cards];
+  work.sort((x, y) => (x >> 2) - (y >> 2)).reverse();
+  const rletter = work.map(rankRepr).join('');
+  const sletter = ((work[0] & 0b11) === (work[1] & 0b11)) ? 's' : 'o';
+  if ((work[0] >> 2) === (work[1] >> 2)) {
+    return rletter; // returns AA, not AAo
+  }
+  return `${rletter}${sletter}`;
+};
+
+const hparse = (str) => { // AKo -> hand,
+  if (str.length === 2) { // pockets
+    return [parse(`${str[0]}H`), parse(`${str[1]}D`)];
+  }
+  switch (str[2]) {
+    case 's': return [parse(`${str[0]}H`), parse(`${str[1]}H`)];
+    case 'o': return [parse(`${str[0]}H`), parse(`${str[1]}D`)];
+    default: throw Error('invalid input');
+  }
+};
+
+module.exports = {
+  card,
+  repr,
+  parse,
+  hrepr,
+  hparse,
+};
--- test/card.test.js
+++ test/card.test.js
@@ -1,4 +1,10 @@
-const { card, repr, parse } = require('../src/card.js');
+const {
+  card,
+  repr,
+  parse,
+  hrepr,
+  hparse,
+} = require('../src/card.js');
 
 test('check card()', () => {
   expect(card(0, 0)).toBe(0);
@@ -23,3 +29,22 @@ test('check parse()', () => {
   expect(parse('As')).toBe(51);
   expect(parse('AS')).toBe(51);
 });
+
+test('check hrepr()', () => {
+  expect(hrepr([card(11, 0), card(11, 1)])).toBe('KK');
+  expect(hrepr([card(11, 0), card(12, 1)])).toBe('AKo');
+  expect(hrepr([card(11, 0), card(12, 0)])).toBe('AKs');
+  expect(hrepr([card(1, 0), card(8, 0)])).toBe('T3s');
+});
+
+test('check hparse()', () => {
+  expect(hparse('AA')).toEqual([card(12, 0), card(12, 1)]);
+  expect(hparse('AKs')).toEqual([card(12, 0), card(11, 0)]);
+  expect(hparse('AKo')).toEqual([card(12, 0), card(11, 1)]);
+
+  expect(hparse('33')).toEqual([card(1, 0), card(1, 1)]);
+  expect(hparse('34s')).toEqual([card(1, 0), card(2, 0)]);
+  expect(hparse('34o')).toEqual([card(1, 0), card(2, 1)]);
+
+});
+
