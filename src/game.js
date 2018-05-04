3b4b210f75a376c9bfcd71538f3459eefe5bda30
41semicolon <41semicolon@gmail.com>
2018-05-04 12:42:19 +0000

    added some stuff for bot

--- src/game.js
+++ src/game.js
@@ -2,4 +2,9 @@ const { range, shuffle } = require('./util.js');
 
 const deck = () => shuffle(range(52));
 
-module.exports = { deck };
+// XXX: pot has to include opponents' bet
+// i.e. pot must be 100+30*2 when two other opponents already bet/call 30.
+const potodds = (pot, bet) => (pot + bet) / bet;
+const ev = (hsval, pot, bet) => hsval * (pot + bet);
+
+module.exports = { deck, ev, potodds };
