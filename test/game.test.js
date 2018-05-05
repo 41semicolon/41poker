5573a8e73361b54f6c497d8a202f2eb335943471
41semicolon <41semicolon@gmail.com>
2018-05-05 06:23:59 +0000

    fixed testcases

--- test/game.test.js
+++ test/game.test.js
@@ -6,36 +6,35 @@ test('position finding', () => {
   let list = [f, f, f, f, f];
   let last = 0;
   expect(game.positionOf(list, last))
-    .toEqual({ posBTN: 4, posSB: 0, posBB: 1, first: 1 });
+    .toEqual({ posBTN: 4, posSB: 0, posBB: 1 });
   last = 1;
   expect(game.positionOf(list, last))
-    .toEqual({ posBTN: 0, posSB: 1, posBB: 2, first: 2 });
+    .toEqual({ posBTN: 0, posSB: 1, posBB: 2 });
   last = 2;
   expect(game.positionOf(list, last))
-    .toEqual({ posBTN: 1, posSB: 2, posBB: 3, first: 3 });
+    .toEqual({ posBTN: 1, posSB: 2, posBB: 3 });
   last = 3;
   expect(game.positionOf(list, last))
-    .toEqual({ posBTN: 2, posSB: 3, posBB: 4, first: 4 });
+    .toEqual({ posBTN: 2, posSB: 3, posBB: 4 });
   last = 4;
   expect(game.positionOf(list, last))
-    .toEqual({ posBTN: 3, posSB: 4, posBB: 0, first: 0 });
+    .toEqual({ posBTN: 3, posSB: 4, posBB: 0 });
 
 
   list = [f, t, t, f, f];
   last = 0;
   expect(game.positionOf(list, last))
-    .toEqual({ posBTN: 4, posSB: 0, posBB: 3, first: 3 });
+    .toEqual({ posBTN: 4, posSB: 0, posBB: 3 });
   last = 1;
   expect(game.positionOf(list, last))
-    .toEqual({ posBTN: 4, posSB: 0, posBB: 3, first: 3 });
+    .toEqual({ posBTN: 4, posSB: 0, posBB: 3 });
   last = 2;
   expect(game.positionOf(list, last))
-    .toEqual({ posBTN: 4, posSB: 0, posBB: 3, first: 3 });
+    .toEqual({ posBTN: 4, posSB: 0, posBB: 3 });
   last = 3;
   expect(game.positionOf(list, last))
-    .toEqual({ posBTN: 0, posSB: 3, posBB: 4, first: 4 });
+    .toEqual({ posBTN: 0, posSB: 3, posBB: 4 });
   last = 4;
   expect(game.positionOf(list, last))
-    .toEqual({ posBTN: 3, posSB: 4, posBB: 0, first: 0 });
-
+    .toEqual({ posBTN: 3, posSB: 4, posBB: 0 });
 });
