01abd85f9ab373afc26a921c8a93b66913913b11
41semicolon <41semicolon@gmail.com>
2018-05-18 03:35:59 +0000

    added available action

--- src/rule/bet.js
+++ src/rule/bet.js
@@ -14,7 +14,29 @@ const isValidAmount = (BB, history, amount) => {
   return amount >= last + (last - sndlast);
 };
 
+const availableAction = (stack, BB, history) => {
+  const actions = [{ type: 'fold', value: 0 }];
+  const c2c = history.length === 0 ? 0 : history[history.length - 1];
+  // check and call
+  if (c2c === 0) {
+    actions.push({ type: 'check', value: 0 });
+  } else if (c2c <= stack) {
+    actions.push({ type: 'call', value: c2c });
+  }
+  // allin
+  if (stack !== 0) {
+    actions.push({ type: 'allin', value: stack });
+  }
+  // raise
+  const minbet = minimum(BB, history);
+  if (stack >= minbet) {
+    actions.push({ type: 'raise', value: minbet });
+  }
+  return actions;
+};
+
 module.exports = {
   minimum,
   isValidAmount,
+  availableAction,
 };
